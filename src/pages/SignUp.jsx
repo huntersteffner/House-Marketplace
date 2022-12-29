b931808bd65c063d054867b9525d8c80b0840b35
huntersteffner <huntersteffner@gmail.com>
2022-12-29 15:37:15 -0500

    Google Auth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import {toast} from 'react-toastify'
-import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
-import {db} from '../firebase.config'
-import {setDoc, doc, severTimestamp, serverTimestamp} from 'firebase/firestore'
+import { toast } from 'react-toastify'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth'
+import { db } from '../firebase.config'
+import {
+  setDoc,
+  doc,
+  severTimestamp,
+  serverTimestamp,
+} from 'firebase/firestore'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import OAuth from '../components/OAuth'
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false)
@@ -20,9 +30,9 @@ const SignUp = () => {
   const navigate = useNavigate()
 
   const onChange = (e) => {
-    setFormData((prevState) =>({
-        ...prevState,
-        [e.target.id]: e.target.value
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
     }))
   }
 
@@ -30,25 +40,29 @@ const SignUp = () => {
     e.preventDefault()
 
     try {
-        const auth = getAuth()
+      const auth = getAuth()
 
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
 
-        const user = userCredential.user
+      const user = userCredential.user
 
-        updateProfile(auth.currentUser, {
-            displayName: name
-        })
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      })
 
-        const formDataCopy = {...formData}
-        delete formDataCopy.password
-        formDataCopy.timestamp = serverTimestamp()
+      const formDataCopy = { ...formData }
+      delete formDataCopy.password
+      formDataCopy.timestamp = serverTimestamp()
 
-        await setDoc(doc(db, 'users', user.uid), formDataCopy)
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
 
-        navigate('/')
+      navigate('/')
     } catch (error) {
-        toast.error('Something went wrong with registration.')
+      toast.error('Something went wrong with registration.')
     }
   }
   return (
@@ -59,7 +73,7 @@ const SignUp = () => {
         </header>
         <main>
           <form onSubmit={onSubmit}>
-          <input
+            <input
               type="text"
               className="nameInput"
               placeholder="Name"
@@ -93,18 +107,22 @@ const SignUp = () => {
               />
             </div>
 
-            <Link to='/forgot-password' className='forgotPasswordLink' >Forgot Password</Link>
+            <Link to="/forgot-password" className="forgotPasswordLink">
+              Forgot Password
+            </Link>
             <div className="signUpBar">
-                <p className="signUpText">
-                    Sign Up
-                </p>
-                <button className="signUpButton">
-                    <ArrowRightIcon fill='#fff' width='34px' height='34px'/>
-                </button>
+              <p className="signUpText">Sign Up</p>
+              <button className="signUpButton">
+                <ArrowRightIcon fill="#fff" width="34px" height="34px" />
+              </button>
             </div>
           </form>
 
-          <Link to='/sign-in' className='registerLink' >Sign In Instead</Link>
+          < OAuth />
+
+          <Link to="/sign-in" className="registerLink">
+            Sign In Instead
+          </Link>
         </main>
       </div>
     </>
