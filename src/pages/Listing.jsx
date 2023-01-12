7dd9b2ffa321e28cebca66098b618a7dcfd208d4
huntersteffner <huntersteffner@gmail.com>
2023-01-12 15:25:21 -0500

    Contact Page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -92,7 +92,7 @@ const Listing = () => {
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
             to={`/contact/${listing.userRef}?listingName=${listing.name}`}
-            className="primaryButton"
+            className='primaryButton'
           >
             Contact Landlord
           </Link>
